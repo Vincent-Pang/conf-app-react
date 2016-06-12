6ffd94899ff67d7e37d3a63673dcd21bc97c040a
Yiu Man Chi <ymcatar@gmail.com>
2016-06-12 22:57:17 +0800

    add: production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-/* No production settings for now, only dev! */
+var webpack = require('webpack');
+var _ = require('lodash');
+
+var isProduction = process.env.NODE_ENV === 'production';
+
+var basePlugins = [
+  new HtmlWebpackPlugin({
+    title: 'ConfApp',
+    template: './src/index.ejs'
+  }),
+  new webpack.optimize.OccurrenceOrderPlugin(),
+  new webpack.optimize.DedupePlugin()
+];
 
 module.exports = [{
   cache: true,
@@ -23,11 +35,22 @@ module.exports = [{
       }
     }]
   },
-  devtool: 'eval-cheap-module-inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'ConfApp',
-      template: './src/index.ejs'
-    })
-  ]
+  devtool: 'source-map',
+  plugins: isProduction
+    ? _.concat(basePlugins, [
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        sourceMap: false,
+        mangle: false,
+        removeRedundantAttributes: false,
+        output: { comments: false },
+        compress: { warnings: false }
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }
+      })
+    ])
+    : basePlugins
 }];
